ffcb603b337ed8650812a1a03169b739e9d16520
MinWooPark-dotcom <positive1attraction@gmail.com>
2025-09-25 01:41:48 +0900

    feat: add removing cni script

--- scripts/reset-node.sh
+++ scripts/reset-node.sh
@@ -4,5 +4,12 @@ set -e
 # 클러스터 초기화
 sudo kubeadm reset -f
 
+# CNI 설정 삭제
+sudo rm -rf /etc/cni/net.d
+sudo rm -rf /var/lib/cni
+
 # kubeconfig 삭제
 rm -f ~/.kube/config
+
+# containerd 재시작
+sudo systemctl restart containerd
